e7680f89cd3f562a8949861ae8b964e4090fc6c8
Sam Roberts <saroberwork@outlook.com>
2022-10-27 10:08:19 -0400

    Removed all heroku config

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/drinko-music-0.0.1-SNAPSHOT.jar
--- start_bot.sh
+++
@@ -1,5 +0,0 @@
-export YOUTUBE_USERNAME=""
-export YOUTUBE_PASSWORD=""
-export DISCORD_TOKEN=""
-
-java -jar -Dorg.drinko.youtube.password=$YOUTUBE_PASSWORD -Dorg.drinko.youtube.username=$YOUTUBE_USERNAME -Ddiscord.api.token=$DISCORD_TOKEN drinko-music-0.0.1-SNAPSHOT.jar
